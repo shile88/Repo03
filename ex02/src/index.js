b5110c4dd36f18d5baa6ca3ef4ec599b62837a00
shile88 <milos.ivanis@gmail.com>
2022-03-30 14:47:10 +0200

    Adding comments

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+function main () {
+    // This comment is only for practice 
+
+    /* These comments are only for practice and they
+        are bigger the previous
+    */
+
+}
+
+console.log(main());
+
+module.exports = main;
