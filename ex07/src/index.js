e85b9853ae8c9f78d7183773d883f5737eec446f
shile88 <milos.ivanis@gmail.com>
2022-03-30 15:56:31 +0200

    camelCase practice

---
+++ ex07/src/index.js
@@ -0,0 +1,21 @@
+function main() {
+    // Variable declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    // Variable assignments
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+
+return {
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver,
+    }
+}
+
+console.log(main());
+
+module.exports = main;
